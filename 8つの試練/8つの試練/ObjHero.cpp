9e89cc76f45569429421543423b39836917863ed
h1202019034@hiratagakuen.onmicrosoft.com <h1202019034@hiratagakuen.onmicrosoft.com>
2021-03-01 21:11:49 +0900

    主人公表示

--- 8つの試練/8つの試練/ObjHero.cpp
+++ 8つの試練/8つの試練/ObjHero.cpp
@@ -27,10 +27,26 @@ void CObjHero::Init()
 	m_vx = 0.0f; //移動ベクトル
 	m_vy = 0.0f; //移動ベクトル
 
+	m_direc = HERO_FRONT;
+
+	//正面アニメーション
+	m_ani_time1 = 0; //アニメーションフレーム動作間隔
+	m_ani_frame1 = 0; //静止フレームを初期にする
+
+	//背面アニメーション
+	m_ani_time2 = 0; //アニメーションフレーム動作間隔
+	m_ani_frame2 = 0; //静止フレームを初期にする
+
+	//右アニメーション
+	m_ani_time3 = 0; //アニメーションフレーム動作間隔
+	m_ani_frame3 = 0; //静止フレームを初期にする
+
+	//左アニメーション
+	m_ani_time4 = 0; //アニメーションフレーム動作間隔
+	m_ani_frame4 = 0; //静止フレームを初期にする
 
 	//当たり判定用HitBoxを作成
 	Hits::SetHitBox(this, m_x, m_y, 32, 32, ELEMENT_PLAYER, OBJ_HERO, 1);
-
 }
 
 //アクション
@@ -50,5 +66,68 @@ void CObjHero::Draw()
 	RECT_F src;
 	RECT_F dst;
 
+	int AniData1[3] = //正面向き情報を登録
+	{
+		0,1,2,
+	};
+	int AniData2[3] = //背面向き情報を登録
+	{
+		0,1,2,
+	};
+	int AniData3[2] = //右向き情報を登録
+	{
+		0,1,
+	};
+	int AniData4[2] = //左向き情報を登録
+	{
+		0,1,
+	};
+
+	//主人公
+	if (m_direc == HERO_RIGHT) //右
+	{
+
+		//切り取り位置の設定
+		src.m_top = 64.0f;   //y
+		src.m_left = 0.0f + AniData3[m_ani_frame3] * 32.0f; //x
+		src.m_right = 32.0f + AniData3[m_ani_frame3] * 32.0f; //x
+		src.m_bottom = 96.0f; //y
+
+
+	}
+	if (m_direc ==HERO_BACK) //後ろ
+	{
+		//切り取り位置の設定
+		src.m_top = 32.0f;   //y
+		src.m_left = 0.0f + AniData2[m_ani_frame2] * 32.0f; //x
+		src.m_right = 32.0f + AniData2[m_ani_frame2] * 32.0f; //x
+		src.m_bottom = 64.0f; //y
+	}
+
+	if (m_direc == HERO_FRONT) //前
+	{
+		//切り取り位置の設定
+		src.m_top = 0.0f;   //y
+		src.m_left = 0.0f + AniData1[m_ani_frame1] * 32.0f; //x
+		src.m_right = 32.0f + AniData1[m_ani_frame1] * 32.0f; //x
+		src.m_bottom = 32.0f; //y
+	}
+
+	if (m_direc == HERO_LEFT)//左
+	{
+		//切り取り位置の設定
+		src.m_top = 96.0f;   //y
+		src.m_left = 0.0f + AniData4[m_ani_frame4] * 32.0f; //x
+		src.m_right = 32.0f + AniData4[m_ani_frame4] * 32.0f; //x
+		src.m_bottom = 128.0f; //y
+	}
+
+	//表示位置の設定
+	dst.m_top = 0.0f + m_y;
+	dst.m_left = 0.0f + m_x;
+	dst.m_right = 32.0f + m_x;
+	dst.m_bottom = 32.0f + m_y;
+
+	Draw::Draw(0, &src, &dst, c, 0.0f);
 
 }
--- 8つの試練/8つの試練/ObjHero.h
+++ 8つの試練/8つの試練/ObjHero.h
@@ -4,6 +4,12 @@
 //使用するネームスペース
 using namespace GameL;
 
+#define HERO_FRONT (0)//正面
+#define HERO_BACK  (1)//背面
+#define HERO_RIGHT (2)//右
+#define HERO_LEFT  (3)//左
+
+
 //オブジェクト : 主人公
 class CObjHero : public CObj
 {
@@ -22,5 +28,23 @@ private:
 	float m_y;  //主人公機のy方向移動用変数
 	float m_vx; //移動ベクトル
 	float m_vy; //移動ベクトル
+
+	int m_direc; //主人公の向き
+
+	//正面アニメーション
+	int m_ani_time1; //アニメーションフレーム動作間隔
+	int m_ani_frame1; //描画フレーム
+
+	//背面アニメーション
+	int m_ani_time2; //アニメーションフレーム動作間隔
+	int m_ani_frame2; //描画フレーム
+
+	//右アニメーション
+	int m_ani_time3; //アニメーションフレーム動作間隔
+	int m_ani_frame3; //描画フレーム
+
+	//左アニメーション
+	int m_ani_time4; //アニメーションフレーム動作間隔
+	int m_ani_frame4; //描画フレーム
 
 };
