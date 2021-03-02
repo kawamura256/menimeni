1e5694060c9dc4e808b2e2bcda9066c9257841df
ryo12281456@outlook.jp <ryo12281456@outlook.jp>
2021-03-02 10:48:33 +0900

    hiroma

--- 8つの試練/8つの試練/ObjHall.cpp
+++ 8つの試練/8つの試練/ObjHall.cpp
@@ -2,6 +2,7 @@
 #include"GameL/DrawTexture.h"
 #include"GameL/WinInputs.h"
 #include"GameL/SceneManager.h"
+#include"GameL/SceneObjManager.h"
 
 #include "GameHead.h"
 #include "ObjHall.h"
@@ -57,7 +58,87 @@ void CObjHall::Init()
 //アクション
 void CObjHall::Action()
 {
-	
+	//主人公の位置を取得
+	CObjHero* hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
+	float hx = hero->GetX();
+	float hy = hero->GetY();
+
+	//m_mapの全要素にアクセス
+	for (int i = 0; i < 35; i++)
+	{
+		for (int j = 0; j < 35; j++)
+		{
+			if (m_map[i][j] > 0)
+			{
+				//要素番号を座標に変更
+				float x = j * 64.0f;
+				float y = i * 64.0f;
+
+				//主人公とブロックの当たり判定
+				if ((hx + 64.0f > x) && (hx < x + 64.0f) && (hy + 64.0f > y) && (hy < y + 64.0f))
+				{
+					//上下左右判定
+
+					//vectorの作成
+					float vx = hx - x;
+					float vy = hy - y;
+
+					//長さを求める
+					float len = sqrt(vx * vx + vy * vy);
+
+					//角度を求める
+					float r = atan2(vy, vx);
+					r = r * 180.0f / 3.14f;
+
+					if (r <= 0.0f)
+						r = abs(r);
+					else
+						r = 360.0f - abs(r);
+
+					//lenがある一定の長さのより短い場合判定に入る
+					if (len < 88.0f)
+					{
+						//角度で上下左右を判定
+						if ((r < 45 && r>0) || r > 315)
+						{
+							//右
+							hero->SetRight(true);//主人公から見て、左の部分が衝突している
+							hero->SetX(x + 64.0f + (m_scroll));//ブロックの位置-主人公の幅
+							hero->SetVX(-hero->GetVX() * 0.1f); //-VX*反発係数
+						}
+						if (r > 45 && r < 135)
+						{
+							//上
+							hero->SetDown(true);//主人公から見て、下の部分が衝突している
+							hero->SetY(y - 64.0f);//ブロックの位置-主人公の幅
+							//種類を渡すのスタートとゴールのみ変更する
+							if (m_map[i][j] == 6 || m_map[i][j] == 7)
+								hero->SetBT(m_map[i][j]);//ブロックの要素(type)を主人公に渡す
+							hero->SetVY(0.0f);
+						}
+						if (r > 135 && r < 225)
+						{
+							//左
+							hero->SetLeft(true);//主人公から見て、右の部分が衝突している
+							hero->SetX(x - 64.0f + (m_scroll));//ブロックの位置-主人公の幅
+							hero->SetVX(-hero->GetVX() * 0.1f);//-VX*反発係数
+						}
+						if (r > 225 && r < 315)
+						{
+							//下
+							hero->SetUp(true);//主人公から見て、上の部分が衝突している
+							hero->SetY(y + 64.0f);//ブロックの位置-主人公の幅
+							if (hero->GetVY() < 0)
+							{
+								hero->SetVY(0.0f);
+							}
+						}
+						
+					}
+				}
+			}
+		}
+	}
 }
 
 //ドロー
--- 8つの試練/8つの試練/ObjHero.h
+++ 8つの試練/8つの試練/ObjHero.h
@@ -34,6 +34,16 @@ public:
 	float GetX() { return m_x; }
 	float GetY() { return m_y; }
 
+	void SetX(float x) { m_x = x; }
+	void SetY(float y) { m_y = y; }
+	void SetVY(float vy) { m_vy = vy; }
+	void SetVX(float vx) { m_vx = vx; }
+
+	void SetUp(bool b) { m_hit_up = b; }
+	void SetDown(bool b) { m_hit_down = b; }
+	void SetLeft(bool b) { m_hit_left = b; }
+	void SetRight(bool b) { m_hit_right = b; }
+
 private:
 
 	float m_x;  //主人公機のx方向移動用変数
@@ -58,5 +68,10 @@ private:
 	//左アニメーション
 	int m_ani_time4; //アニメーションフレーム動作間隔
 	int m_ani_frame4; //描画フレーム
+
+	bool m_hit_up;
+	bool m_hit_down;
+	bool m_hit_left;
+	bool m_hit_right;
 
 };
